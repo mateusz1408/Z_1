35d989346c5474d95d4629348d69b41e70ea8d33
mateusz1408 <162459899+mateusz1408@users.noreply.github.com>
2024-03-20 13:25:12 +0000

    Z_1v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,12 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    String str = "Hello World";
+    for(int i=0; i<str.length(); i++){
+      System.out.println(str.charAt(i));
+    }
+    
+   
   }
 
   // @Test
